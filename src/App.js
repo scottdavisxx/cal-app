5e4374f472959c579192b467690acdc733116905
Scott Davis <scottdavisxx@gmail.com>
2022-03-25 22:46:51 -0400

    add calendar component, populate with basic data

--- src/App.js
+++ src/App.js
@@ -8,12 +8,53 @@ import "react-big-calendar/lib/css/react-big-calendar.css";
 import React, {useState} from "react";
 import DatePicker from "react-datepicker"
 
+const locales = {
+  "en-US": require("date-fns/locale/en-US")
+}
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+})
+
+// Dummy Date
+const events = [
+  {
+    title: "Big Meeting",
+    allDay: true,
+    start: new Date(2022, 3, 1),
+    end: new Date(2022, 3, 1)
+
+  },
+  {
+    title: "Heart Transplant",
+    start: new Date(2022, 3, 1),
+    end: new Date(2022, 3, 1)
+    
+  },
+  {
+    title: "Seance",
+    start: new Date(2022, 3, 1),
+    end: new Date(2022, 3, 1)
+    
+  }
+]
+
+
 
 
 function App() {
   return (
     <div className="App">
-      
+      <Calendar 
+        localizer={localizer} 
+        events={events} 
+        startAccessor="start" 
+        endAccessor="end"
+        style={{height: 500, margin: "50px"}} />
     </div>
   );
 }
